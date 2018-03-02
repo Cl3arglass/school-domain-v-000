302b81d02b4500df0184fa30560fd8d15f6e6303
Holden Altaffer <haltaffer@gmail.com>
2018-03-02 23:01:33 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+attr_reader :name, :roster, :add_student
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student (name, grade)
+
+  @roster[grade] ||= []
+  @roster[grade] << name
+end
+
+end
+
+# attr_reader :roster, :add_student
+#
+#
+#   def initialize(roster)
+#     @roster = roster
+#     roster.clear
+#   end
+#
+#   def add_student=(add_student, num)
+#     @add_student = add_student
+#     roster[num] = []
+#     roster[num] << add_student
